de737942d97ee1b54e2eb1de64f8124d6189533d
Anthony Marion <anthonycmarion@gmail.com>
2023-06-11 12:32:01 +1000

    Added Images controller with all CRUD actions

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,40 @@
 class ImagesController < ApplicationController
+
+  # INDEX ALL IMAGES/HOMEPAGE
+  def index
+    @images = Image.all
+  end
+
+  # CREATE NEW INSTANCE OF IMAGE
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+    @image.user = current_user
+    if @image.save
+      redirect_to images_path, notice: 'Image successfully uploaded'
+    else
+      # RENDER NEW IMAGE UPLOAD IF UNSUCCESSFUL
+      render :new
+  new
+
+  # SHOW IMAGE
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  # DELETE IMAGE
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+    redirect_to images_path, notice: 'Image deleted'
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:title, :date_uploaded, :image)
+  end
 end
