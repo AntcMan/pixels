ffdb221c814790a952b39c570ae5db73ed1dcdaf
Anthony Marion <anthonycmarion@gmail.com>
2023-06-14 18:28:24 +1000

    Added event code to Image Modal stimulus controller; updated Index.html with target

--- app/javascript/controllers/image_modal_controller.js
+++ app/javascript/controllers/image_modal_controller.js
@@ -3,5 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="image-modal"
 export default class extends Controller {
   connect() {
+    console.log("ImageModal controller connected");
+    this.element.addEventListener('click', this.showModal)
+  }
+
+  showModal(e) {
+    e.preventDefault();
+    const imageUrl = e.target.getAttribute();
+
   }
 }
